8a337a089314245702dc501ff9cdc0b8802db5ec
srikanthpragada <srikanthpragada@gmail.com>
2016-04-04 20:31:49 +0530

    Updated

---
+++ WebContent/update_salary.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="s" uri="/struts-tags"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Update Salary</title>
+</head>
+<body>
+	<h1>Update Salary</h1>
+	<s:form action="updateSalary">
+	   <s:textfield name="empid" label="Employee Id"></s:textfield>
+	   <s:textfield name="salary" label="Salary"></s:textfield>
+	   <s:submit value="Update Salary"></s:submit>
+	</s:form>
+    <h3>
+      <s:property value="message" />
+    </h3>
+</body>
+</html>
---
+++ src/action/UpdateSalaryAction.java
@@ -0,0 +1,71 @@
+package action;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UpdateSalaryAction extends ActionSupport {
+
+	private int empid, salary;
+	private String message;
+
+	public int getEmpid() {
+		return empid;
+	}
+
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String execute() {
+//		System.out.println(empid);
+//		System.out.println(salary);
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		}
+		catch(Exception ex) {
+		    message = "Sorry! Unable to connect to database!";
+		    return  "failure";
+		}
+
+		
+		// connect to Oracle
+		try (Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "hr")) {
+			PreparedStatement ps = con.prepareStatement("update employees set salary =? where employee_id = ?");
+			ps.setInt(1, salary);
+			ps.setInt(2, empid);
+
+			int count = ps.executeUpdate();
+
+			if (count == 1)
+				message = "Updated Successfully!";
+			else
+				message = "Sorry! Employee not found!";
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+			message = "Sorry! Some error prevented the process!";
+		}
+		
+		return SUCCESS;
+	}
+
+}
